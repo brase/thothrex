eba2bf6f317152efff135fa27fdbdc412073e082
Sebastian Brandt <bastibrandt@gmail.com>
2019-05-13 07:26:02 +0200

    example wich shows thoth failing

---
+++ Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Thoth.Json.Net
+
+type MyRecType =
+    { Name: string
+      Children: MyRecType List }
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let model = { Name = "This will fail"
+                  Children = [] }
+    let json = Encode.Auto.toString(0, model)
+
+    sprintf "%s" json
+    |> ignore
+
+    0 // return an integer exit code
